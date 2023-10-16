0e63acb9e456ad1d0571e0f42c7d3d5be9343ca1
Philip Müller <philipmuller97@gmail.com>
2023-10-17 01:48:10 +0300

    Add pdf-to-text with routing to deck page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import Link from "next/link";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useCallback } from "react";
 import { motion, motionValue } from "framer-motion";
+import ConvertApi from "convertapi-js";
+import { convertAPISecret } from "@/keychain";
+import { useRouter, useSearchParams} from 'next/navigation'
+
 
 export default function Home() {
   const [cardNr, setCardNr] = useState(3);
   const [coords, setCoords] = useState({x: 0, y: 0});
   const [isHoveringFile, setIsHoveringFile] = useState(false);
+
+  const router = useRouter();
+  const searchParams = useSearchParams()!;
 
   useEffect(() => {
     const handleWindowMouseMove = (event: { clientX: any; clientY: any; }) => {
@@ -48,6 +55,16 @@ export default function Home() {
     return rotation;
   }
 
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams)
+      params.set(name, value)
+ 
+      return params.toString()
+    },
+    [searchParams]
+  )
+
   const offsetsY = [90, -140, 0];
   const fileOffsetsY = [250, 300, 350];
 
@@ -61,18 +78,73 @@ export default function Home() {
     hover: { opacity: 1 }
   };
 
-  function handleDragEnter(event: { preventDefault: () => void; }) {
+  function handleDragEnter(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
     console.log("drag enter");
     setIsHoveringFile(true);
   }
 
-  function handleDragExit(event: { preventDefault: () => void; }) {
+  function handleDragExit(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
     console.log("drag exit");
     setIsHoveringFile(false);
   }
 
+  function handleDragOver(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsHoveringFile(true);
+  }
+
+  async function handleDrop(e: any) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("File has been dropped");
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      const convertApi = ConvertApi.auth({ apiKey: "111029228", token: "r2mjY2uK"});
+      let params = convertApi.createParams();
+      //const convertapi = require('convertapi')(convertAPISecret);
+      const file = e.dataTransfer.files[0];
+      console.log(file);
+
+      params.add('file', file);
+      let result = await convertApi.convert("pdf", "txt", params);
+
+      let url = result.files[0].Url;
+      console.log(url);
+
+      fetch(url).then(function(response) {
+        response.text().then(function(text) {
+          router.push("/deck" + '?' + createQueryString("fileText", text));
+        });
+  });
+    }
+  }
+
+  function handleChange(e: any) {
+    e.preventDefault();
+    console.log("File has been added");
+    if (e.target.files && e.target.files[0]) {
+      for (let i = 0; i < e.target.files["length"]; i++) {
+        console.log(e.dataTransfer.files[i]);
+        //setFiles((prevState: any) => [...prevState, e.target.files[i]]);
+      }
+    }
+  }
+
   return (
-    <section className="flex-grow flex flex-col items-center justify-center" onDragEnter={handleDragEnter} onDragLeave={handleDragExit}>
+    <form className="flex-grow flex flex-col items-center justify-center" onDragEnter={handleDragEnter} onDragLeave={handleDragExit} onDrop={handleDrop} onDragOver={handleDragOver}>
+      <input
+        placeholder="fileInput"
+        className="hidden"
+        //ref={inputRef}
+        type="file"
+        multiple={true}
+        //onChange={handleChange}
+        accept=".xlsx,.xls,image/*,.doc, .docx,.ppt, .pptx,.txt,.pdf"
+      />
       <motion.div initial={{ visibility: "hidden" }} whileInView={{ visibility: isHoveringFile ? "visible" : "hidden" }}><h1 className="text-3xl text-slate-600">Drop file to create cards</h1></motion.div>
       <div className="flex -space-x-32">
         {Array.from({ length: cardNr }).map((_, idx) => (
@@ -90,6 +162,6 @@ export default function Home() {
         <p className="mt-60 text-lg text-gray-700">Coords {coords.x} {coords.y}</p>
         <button onClick={() => setCardNr(cardNr+1)}>Click me</button>
       </div>
-    </section>
+    </form>
   );
 }
--- keychain.ts
+++ keychain.ts
@@ -1,3 +1,4 @@
 //This file contains sensitive information and should not be committed to git.
 
 export const openAIKey = "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";
+export const convertAPISecret = "WkVVq7nqAt12WN52";
