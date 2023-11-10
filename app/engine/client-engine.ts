d9b8cc6f326b6760c7b4f6c203e864e8ad06fc07
Philip Müller <philipmuller97@gmail.com>
2023-11-10 11:32:42 +0200

    Remove useless node-fetch and fix lantern

--- app/engine/client-engine.ts
+++ app/engine/client-engine.ts
@@ -1,10 +1,6 @@
 import { Logger } from "./logging-engine";
-import { LandingPageManager } from "./page-managers/landing-page";
 
 export abstract class LanternEngine {
     static logger: Logger = new Logger("LanternEngine");
-
-    //page managers
-    static landing: typeof LandingPageManager = LandingPageManager;
     
 }
