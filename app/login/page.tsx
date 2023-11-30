fef70d891a0d56677f500dce0e0ee8c6dd6a76b3
raluciure <ralu.ciure@gmail.com>
2023-11-30 19:19:59 +0100

    Created login and signup layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,11 +3,14 @@
 import React, { FormEvent, useRef } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../engine/firebase";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
 
     const loginEmailRef = useRef<HTMLInputElement>(null);
     const loginPasswordRef = useRef<HTMLInputElement>(null);
+    const router = useRouter();
 
     // maybe we can move it to engine
     const login = (e: FormEvent<HTMLFormElement>) => {
@@ -15,14 +18,17 @@ export default function Login() {
 
         const email: string = loginEmailRef.current?.value !== undefined ? loginEmailRef.current?.value : ''
         const password: string = loginPasswordRef.current?.value !== undefined ? loginPasswordRef.current?.value : ''
-
-
+        
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
-                
+
                 const user = userCredential.user;
                 console.log(user);
+                
+                router.push('/')
                 alert("Login successful!");
+                
+
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -32,17 +38,31 @@ export default function Login() {
     }
 
     return (
-        <center className="mt-8">
-            <h1>
-                Login
-                <br />
-                <br />
-            </h1>
-            <form onSubmit={login}>
-                <input type='email' placeholder="Enter email" ref={loginEmailRef} /> <br />
-                <input type='password' placeholder="Enter password" ref={loginPasswordRef} /> <br />
-                <button type='submit'>Login</button>
-            </form>
-        </center>
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Sign in to your account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" onSubmit={login}>
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" ref={loginEmailRef} />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" ref={loginPasswordRef} />
+                            </div>
+                            <button type="submit" className="w-full bg-yellow-500 hover:bg-yellow-600 text-white py-1 px-4 rounded">Log In</button>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Don’t have an account yet? <Link href="/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,14 +1,28 @@
 "use client";
 
-import React, { FormEvent, FormEventHandler, MouseEvent, useRef } from "react";
-import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import React, { FormEvent, MouseEvent, useEffect, useRef } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, getRedirectResult, onAuthStateChanged, signInWithRedirect } from "firebase/auth";
 import { auth } from "../engine/firebase";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Signup() {
 
     const emailRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
     const provider = new GoogleAuthProvider();
+    const router = useRouter();
+
+    // check if user is logged in
+    useEffect(() => onAuthStateChanged(auth, (user) => {
+        if (user) {
+            const uid = user.uid;
+            router.push('/');
+        } else {
+            // User is signed out
+            console.log("User is signed out.")
+        }
+    }))
 
     // maybe we can move it to engine
     const signup = (e: FormEvent<HTMLFormElement>) => {
@@ -21,8 +35,8 @@ export default function Signup() {
             .then((userCredential) => {
                 // Signed up 
                 const user = userCredential.user;
-                alert("You signed up successfully!")
-
+                router.push('/');
+                alert("You signed up successfully!");
             })
             .catch((error) => {
                 const errorCode = error.code;
@@ -34,15 +48,19 @@ export default function Signup() {
     // maybe we can move it to engine
     const googleSignIn = (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        signInWithPopup(auth, provider)
+        const auth = getAuth();
+        signInWithRedirect(auth, provider);
+        getRedirectResult(auth)
             .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential?.accessToken;
+                // This gives you a Google Access Token. You can use it to access Google APIs.
+                if (result != null) {
+                    const credential = GoogleAuthProvider.credentialFromResult(result);
+                    const token = credential?.accessToken;
+                }
                 // The signed-in user info.
-                const user = result.user;
-                // IdP data available using getAdditionalUserInfo(result)
-                alert("Signed in with Google successful!")
+                const user = result?.user;
+                console.log(user);
+                alert("Sign up with Google successful!");
             }).catch((error) => {
                 // Handle Errors here.
                 const errorCode = error.code;
@@ -51,24 +69,58 @@ export default function Signup() {
                 const email = error.customData.email;
                 // The AuthCredential type that was used.
                 const credential = GoogleAuthProvider.credentialFromError(error);
-                alert(error.message);
+                alert(errorMessage);
+
             });
     }
 
 
     return (
-         <center className="mt-8">
-            <h1>
-                Signup
-                <br />
-                <br />
-            </h1>
-            <form onSubmit={signup}>
-                <input type='email' placeholder="Enter email" ref={emailRef} /> <br />
-                <input type='password' placeholder="Enter password" ref={passwordRef} /> <br />
-                <button type='submit'>Signup</button> <br/>
-                <button onClick={googleSignIn}>Sign in using Google</button>
-            </form>
-        </center>
+        <section className="mt-8 bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Create your account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" onSubmit={signup}>
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" ref={emailRef} />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" ref={passwordRef} />
+                            </div>
+                            <div>
+                                <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            </div>
+                            <button type="submit" className="w-full bg-yellow-500 hover:bg-yellow-600 text-white py-1 px-4 font-medium rounded-lg">Create an account</button>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Already have an account? <Link href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
+                            </p>
+                        </form>
+                        <div
+                            className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
+                            <p
+                                className="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
+                                OR
+                            </p>
+                        </div>
+
+                        <div className="px-6 sm:px-0 max-w-sm">
+                            <button type="button" className="text-white w-full  bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center justify-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2" onClick={googleSignIn}>
+                                <svg className="mr-2 -ml-1 w-4 h-4 items-center justify-center" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
+                                    <path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
+                                </svg>
+                                Sign up with Google
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
